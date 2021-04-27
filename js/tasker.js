29c88c9e9319dc8b3ef5daf4000d461ec9199363
Abhijeet Roy <ajroysstyle@gmail.com>
2021-04-27 19:12:40 +0530

    first commit

--- js/tasker.js
+++ js/tasker.js
@@ -1,18 +1,22 @@
 function openApp(name){
     switch (name) {
         case 'whatsapp':
-            window.open(`whatsapp://send?phone=+918058523111&text=Hello, I just wanted to say you something`);
+            window.open(`whatsapp://send?phone=+918058523111&text=Hello, I just wanted to say you something`,taget='_self');
             break;
         case 'instagram':
-            window.open('instagram://user?username=roy__galaxy');
+            window.open('instagram://user?username=roy__galaxy',taget='_self');
             break;
         case 'email':
-            window.open('mailto:ajroyssttle@gmail.com?Subject=Registration&body=Your text here...')
+            window.open('mailto:ajroyssttle@gmail.com?Subject=Registration&body=Your text here...',taget='_self')
         default:
             // code
     }
 }
 
-function openForm(){
-    window.open("login/login.html");
+function launch(item){
+    window.open(`${item}/index.html`,taget='_self')
+}
+
+function view(id){
+    document.getElementById(`${id}`).scrollIntoView({behavior:'smooth'});
 }
