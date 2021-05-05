cbc220b489fec8f3be6115049517633294910fc2
Abhijeet Roy <ajroysstyle@gmail.com>
2021-05-05 17:28:06 +0530

    first commit

--- Account-Manager/tasker.js
+++ Account-Manager/tasker.js
@@ -11,14 +11,15 @@ const total = document.querySelector('#total');
 // On form Submit
 form.addEventListener("submit",
     () => {
-        let n = (localStorage.length)/3;
+        let n = (localStorage.length - 1)/3;
         
         localStorage[`name${n}`] = name.value;
         localStorage[`rate${n}`] = rate.value;
         localStorage[`qty${n}`] = qty.value;
-        
-        console.log(localStorage);
+        entries.push(n)
         clear();
+        
+        localStorage.setItem('list',JSON.stringify(entries));
     }
 );
 
@@ -27,6 +28,8 @@ const clear = () => {
     name.value = '';
     rate.value = '';
     qty.value = '';
+    
+    name.focus();
 }
 
 // Show Popup
@@ -36,23 +39,52 @@ listbtn.onclick = () => {
 }
 
 // Make list
+var entries = [];
 function createList(){
+    getList();
     list.innerHTML = '';
     var sum = 0;
-    for(var i = 0;i < localStorage.length/3; i++){
-        let name = localStorage[`name${i}`];
-        let rate = localStorage[`rate${i}`];
-        let qty = localStorage[`qty${i}`];
-        list.innerHTML += `<li>${i+1}. ${name} (${qty}) <span>${rate * qty}</span></li>`;
+    for(var i = 0;i < entries.length; i++){
+        let name = localStorage[`name${entries[i]}`];
+        let rate = localStorage[`rate${entries[i]}`];
+        let qty = localStorage[`qty${entries[i]}`];
+        const li = `<li>${i+1}. ${name} (${qty}) <span><span id="danger" class="${i}" onclick="del(this)">Delete</span> ${rate * qty}</span></li>`;
+        
+        list.innerHTML += li
         sum += rate * qty;
     }
     
     total.innerHTML = `total: ${sum}`
-    
 }
 
 // Clear Local Storage
 function emptyList(){
     localStorage.clear();
+    entries = [];
     createList();
+
+}
+
+function del(el){
+    const num = parseInt(el.className)
+    
+    localStorage.removeItem(`name${num}`);
+    localStorage.removeItem(`rate${num}`);
+    localStorage.removeItem(`qty${num}`);
+    
+    entries.splice(num,1);
+    localStorage.setItem('list',JSON.stringify(entries));
+    createList();
+    
+}
+
+function getList(){
+    var list = localStorage.getItem('list');
+    if(list == undefined){
+        return 2+4
+    }
+    else{
+        entries = JSON.parse(list);
+        console.log(list)
+    }
 }
