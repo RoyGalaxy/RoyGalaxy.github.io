e664c000b0ad04e25906502d8b431ba9769fba3a
RoyGalaxy <ajroysstyle@gmail.com>
2021-05-16 14:33:41 +0530

    first

--- games/notification/tasker.js
+++
@@ -1,9 +0,0 @@
-if('Notification' in window){
-    if(Notification.permission !== 'denied'){
-        let text = 'hello';
-        let opt = {
-            body:'Hi I am Roy Galaxy'
-        }
-        let n = new Notification(text,opt);
-    }
-}
