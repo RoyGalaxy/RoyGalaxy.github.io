8c47fdee68a9bec100c5771a1ebf425c5da07d14
RoyGalaxy <ajroysstyle@gmail.com>
2021-05-27 22:56:30 +0530

    first commit

--- Calculator/sw.js
+++ Calculator/sw.js
@@ -1,3 +1,23 @@
+const staticCacheName = 'static-web';
+const assets = [
+    './index.html',
+    './css/style.css',
+    './js/app.js',
+    './js/tasker.js'
+    ]
+
+
+self.addEventListener('install',(evt) => {
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            cache.addAll(assets);
+        })
+    )
+})
+
+
 self.addEventListener('fetch',(evt) => {
-    console.log('fetch recieved',evt)
+    caches.match(evt.request).then((res) => {
+        return res || fetch(evt.request)
+    })
 });
